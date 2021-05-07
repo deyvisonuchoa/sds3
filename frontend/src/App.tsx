89f1ba6ed86c4a112a1dbf73ccf4b9f77d636401
deyvisonuchoa <deyvisonuchoa@hotmail.com>
2021-05-06 23:18:41 -0300

    static chart

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "components/BarChart";
 import Datatable from "components/DataTable";
+import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import Header from "components/Header";
 
@@ -6,7 +8,25 @@ function App() {
   return (
     <>
       <Header />
-      <Datatable />
+      <div className="container">
+        <h1 className="text-primary py-3"> Dashboard de vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+          <h5 className="text-center text-seocndary">Taxa de sucesso</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-seocndary">Taxa de sucesso</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Totas as vendas</h2>
+        </div>
+        <Datatable />
+      </div>
       <Footer />
     </>
   );
