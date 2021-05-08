37ec58314cedc2ef405c87b6e56031fcb39a15e6
deyvisonuchoa <deyvisonuchoa@hotmail.com>
2021-05-08 18:07:59 -0300

    routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,10 @@
-import BarChart from "components/BarChart";
-import Datatable from "components/DataTable";
-import DonutChart from "components/DonutChart";
-import Footer from "components/Footer";
-import Header from "components/Header";
+import Routes from "Routes";
+
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="container">
-        <h1 className="text-primary py-3"> Dashboard de vendas</h1>
-
-        <div className="row px-3">
-          <div className="col-sm-6">
-          <h5 className="text-center text-seocndary">Taxa de sucesso</h5>
-            <BarChart />
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-seocndary">Taxa de sucesso</h5>
-            <DonutChart />
-          </div>
-        </div>
-
-        <div className="py-3">
-          <h2 className="text-primary">Totas as vendas</h2>
-        </div>
-        <Datatable />
-      </div>
-      <Footer />
+      <Routes />
     </>
   );
 }
---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,33 @@
+import BarChart from "components/BarChart";
+import Datatable from "components/DataTable";
+import DonutChart from "components/DonutChart";
+import Footer from "components/Footer";
+import Header from "components/Header";
+
+export default function Dashboard() {
+    return (
+        <>
+            <Header />
+            <div className="container">
+                <h1 className="text-primary py-3"> Dashboard de vendas</h1>
+
+                <div className="row px-3">
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-seocndary">Taxa de sucesso</h5>
+                        <BarChart />
+                    </div>
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-seocndary">Taxa de sucesso</h5>
+                        <DonutChart />
+                    </div>
+                </div>
+
+                <div className="py-3">
+                    <h2 className="text-primary">Totas as vendas</h2>
+                </div>
+                <Datatable />
+            </div>
+            <Footer />
+        </>
+    )
+}
