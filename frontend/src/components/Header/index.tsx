e4cb1988574db78cc63141ff173261bb4787c227
deyvisonuchoa <deyvisonuchoa@hotmail.com>
2021-05-08 18:14:53 -0300

    Home, navigation

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 
-import imgDsDark from '../../assets/img/ds-dark.svg'; 
+import { Link } from 'react-router-dom';
+import imgDsDark from '../../assets/img/ds-dark.svg';
 
 export default function Header() {
     return (
@@ -7,7 +8,9 @@ export default function Header() {
             <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
                 <div className="container">
                     <nav className="my-2 my-md-0 mr-md-3">
-                        <img src={imgDsDark} alt="DevSuperior" width="120" />
+                        <Link to="/">
+                            <img src={imgDsDark} alt="DevSuperior" width="120" />
+                        </Link>
                     </nav>
                 </div>
             </div>
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,24 @@
+import Footer from "components/Footer";
+import Header from "components/Header";
+import { Link } from "react-router-dom";
 
 
 export default function Home() {
     return (
         <>
-            
+            <Header />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">DSVendas</h1>
+                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <hr />
+                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                    <Link className="btn btn-primary btn-lg" to="/dashboard">
+                        Aessar o Dashboard
+                    </Link>
+                </div>
+            </div>
+            <Footer />
         </>
     )
 }
